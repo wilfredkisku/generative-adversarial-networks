4c0f75d1bbb4f8f8ebd0fb78d0fe2bf84dd7839b
wilfredkisku <wilfred.kisku@gmail.com>
2020-12-06 00:04:54 +0530

    changes in dcgan

--- dcgan.py
+++ dcgan.py
@@ -11,9 +11,15 @@ import time
 
 from IPython import display
 
+
+EPOCHS = 50
+noise_dim = 100
+num_examples_to_generate = 16
 BUFFER_SIZE = 60000
 BATCH_SIZE = 256
 
+#using the seed overtime to visualize the progress in the animated gif
+seed = tf.random.normal([num_examples_to_generate, noise_dim])
 
 def make_generator_model():
     model = tf.keras.Sequential()
@@ -63,9 +69,50 @@ def discriminator_loss(real_output, fake_output):
     total_loss = real_loss + fake_loss
     return total_loss
 
+#helper function for evaluating the cross entropy
 def cross_entropy():
     cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True) 
     return cross_entropy
+
+@tf.function
+def train_step(images):
+    noise = tf.random.normal([BATCH_SIZE, noise_dim])
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = generator(noise, training=True)
+
+        real_output = discriminator(images, training=True)
+        fake_output = discriminator(generated_images, training=True)
+
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+def train(dataset, epochs):
+    for epoch in range(epochs):
+        start = time.time()
+
+    for image_batch in dataset:
+        train_step(image_batch)
+
+    # Produce images for the GIF as we go
+    display.clear_output(wait=True)
+    generate_and_save_images(generator, epoch + 1, seed)
+
+    # Save the model every 15 epochs
+    if (epoch + 1) % 15 == 0:
+        checkpoint.save(file_prefix = checkpoint_prefix)
+
+    print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+
+    # Generate after the final epoch
+    display.clear_output(wait=True)
+    generate_and_save_images(generator, epochs, seed)
 
 
 if __name__ == '__main__':
@@ -95,4 +142,10 @@ if __name__ == '__main__':
     #print(type(cross_entropy()))
     #print(type(tf.keras.losses.BinaryCrossentropy(from_logits=True)))
 
-    
+    generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+    checkpoint_dir = './data'
+    checkpoint_prefic = os.path.join(checkpoint_dir,'ckpt')
+    checkpoint = tf.train.Checkpoint(generator_optimizer = generator_optimizer, discriminator_optimizer = discriminator_optimizer, generator = generator, discriminator = discriminator)
+
