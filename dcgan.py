e75a75db5672413cc8b099bac0b9776d17b7cd14
wilfredkisku <wilfred.kisku@gmail.com>
2020-12-06 20:39:53 +0530

    changes in dcgan

--- dcgan.py
+++ dcgan.py
@@ -114,6 +114,20 @@ def train(dataset, epochs):
     display.clear_output(wait=True)
     generate_and_save_images(generator, epochs, seed)
 
+def generate_and_save_images(model, epoch, test_input): 
+    # Notice `training` is set to False.
+    # This is so all layers run in inference mode (batchnorm).
+    predictions = model(test_input, training=False)
+
+    fig = plt.figure(figsize=(4,4))
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i+1)
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.axis('off')
+
+    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+    plt.show()
 
 if __name__ == '__main__':
 
@@ -149,3 +163,4 @@ if __name__ == '__main__':
     checkpoint_prefic = os.path.join(checkpoint_dir,'ckpt')
     checkpoint = tf.train.Checkpoint(generator_optimizer = generator_optimizer, discriminator_optimizer = discriminator_optimizer, generator = generator, discriminator = discriminator)
 
+    train(train_dataset, EPOCHS)
